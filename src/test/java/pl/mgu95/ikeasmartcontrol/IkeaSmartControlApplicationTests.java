6f4e57d9cc0d9aeaafcb126bdb37901bd4e5672f
mgu95 <mariuszgusc@outlook.com>
2022-09-09 20:27:43 +0200

    Initial commit

---
+++ src/test/java/pl/mgu95/ikeasmartcontrol/IkeaSmartControlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mgu95.ikeasmartcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkeaSmartControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
