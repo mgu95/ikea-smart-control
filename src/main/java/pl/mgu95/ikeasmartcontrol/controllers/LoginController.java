0ccc310a0305bb0dc4239aa3b5f149528d4ffd66
mgu95 <mariuszgusc@outlook.com>
2022-09-10 04:20:37 +0200

    Added dependencies to jsp, jstl, and tradfri gateway.

---
+++ src/main/java/pl/mgu95/ikeasmartcontrol/controllers/LoginController.java
@@ -0,0 +1,40 @@
+package pl.mgu95.ikeasmartcontrol.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import pl.mgu95.ikeasmartcontrol.services.LoginService;
+
+@Controller
+@SessionAttributes({"ip", "identity", "key"})
+public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage(ModelMap modelMap) {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String showHomePage(ModelMap modelMap, @RequestParam String ip, @RequestParam String identity, @RequestParam String key) {
+        boolean isValidCredentials = loginService.validateCredentials(ip, identity, key);
+
+        if (!isValidCredentials) {
+            modelMap.put("errorMessage", "Invalid Credentials");
+            return "login";
+        }
+
+        modelMap.put("ip", ip);
+        modelMap.put("identity", identity);
+        modelMap.put("key", key);
+
+        return "credentialsConfirmation";
+    }
+
+}
